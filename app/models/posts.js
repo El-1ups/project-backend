ab78c72ee7cad86be13c1c27d4655f137397e611
ktu23 <ktu16m@gmail.com>
2022-01-19 20:45:26 -0800

    first backend commit
    added posts.js, posts_routes.js, and post curl scripts

---
+++ app/models/posts.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const postsSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Posts', postsSchema)
